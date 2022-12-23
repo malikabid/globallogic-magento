4b95ba55bddf7174dc210fdc7fc3905a6eb3d40d
Abid H. Malik <malikabid2006@gmail.com>
2022-12-23 07:18:13 +0530

    url rewrite and custom router

---
+++ app/code/GlobalLogic/Blog/Controller/Router.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace GlobalLogic\Blog\Controller;
+
+use Magento\Framework\App\ActionFactory;
+use Magento\Framework\App\Action\Forward;
+use Magento\Framework\App\RouterInterface;
+use Magento\Framework\App\RequestInterface;
+use GlobalLogic\Blog\Model\Post as PostModel;
+
+class Router implements RouterInterface
+{
+
+    protected $actionFactory;
+    protected $postModel;
+
+    public function __construct(
+        ActionFactory $actionFactory,
+        PostModel $postModel
+    ) {
+        $this->actionFactory = $actionFactory;
+        $this->postModel = $postModel;
+    }
+    public function match(RequestInterface $request)
+    {
+        $identifier = trim($request->getPathInfo(), '/');
+
+        $id = '';
+
+        $finalKey = explode('/', $identifier);
+        $urlKey = end($finalKey);
+
+        $post = $this->postModel->load($urlKey, 'url_key');
+        if ($post->getId()) {
+            $id = $post->getId();
+        }
+
+        if ($id) {
+            $request->setModuleName('blog')->setControllerName('index')->setActionName('view')->setParam('id', $id);
+        }
+
+        return $this->actionFactory->create(
+            Forward::class,
+            ['request' => $request]
+        );
+    }
+}
